f70126bd3c568051ca2b6429942867901be0ff4f
Oktay <oktaycvs61@gmail.com>
2026-07-10 11:22:39 +0300

    feat: sepet modülü iskeletini ekle
    BasketController placeholder endpoint'i eklendi.

---
+++ src/main/java/com/cavus/delivery_food/basket/BasketController.java
@@ -0,0 +1,10 @@
+package com.cavus.delivery_food.basket;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/basket")
+public class BasketController {
+}
